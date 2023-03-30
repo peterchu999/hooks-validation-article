37b9441c18416d66ecef78ed8e25d346f749da32
peterchu999 <37611509+peterchu999@users.noreply.github.com>
2023-03-30 15:37:00 +0700

    basic form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import FormField from './components/FormFields';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Sandwich Pre-Order Form</h1>
+      <form>
+        <FormField label="Name" />
+        <FormField label="Email" />
+        <FormField label="Phone Number" />
+        <FormField label="Sandwich Qty" />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
